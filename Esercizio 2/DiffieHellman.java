0f8d43e1d016045269a5fb788b284be6172710f3
Maxelweb <marianosciacco@gmail.com>
2019-07-19 14:26:11 +0200

    Update DiffieHellman.java

--- Esercizio 2/DiffieHellman.java
+++ Esercizio 2/DiffieHellman.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Classe da completare per l'esercizio 2.
@@ -52,21 +53,93 @@ public class DiffieHellman {
     calcoli.put('b', ThreadSafeB);
 
     // =================================
-    // Configurazione della scalabilità
+    // Configurazione della scalabilità e del lavoro da fare
     // =================================
 
     int NumberOfCores = Runtime.getRuntime().availableProcessors();
-    int NumberOfThreads = NumberOfCores * 4;
+    int NumberOfThreads = NumberOfCores * 8;
+    long ThreadBound = (long) Math.ceil(LIMIT/NumberOfThreads);
 
     ExecutorService EsecutoreDeiThreads = Executors.newFixedThreadPool(NumberOfThreads);
 
     // =================================
-    // Configurazione della quantità di lavoro per ciascun thread
+    // Avvio dei lavori per ciascun thread all'interno di un pool
     // =================================
 
-    
+    for(int i = 0; i < NumberOfThreads; ++i)
+    {
+      EsecutoreDeiThreads.execute(new Calcolatore(ThreadBound * i, ThreadBound * (i+1), p, g, publicA, publicB, calcoli));
+      System.out.println("Esecuzione Thread per [" + ThreadBound*i + ", "+ ThreadBound*(i+1) + "]");
+    }
+
+    EsecutoreDeiThreads.shutdown();
+
+    try
+    {
+      EsecutoreDeiThreads.awaitTermination(1, TimeUnit.MINUTES);
+    }
+    catch (InterruptedException e)
+    {
+      System.out.println("Un thread ci ha messo troppo tempo per terminare i calcoli.");
+    }
 
     // =================================
+    // Verifico e reperisco i risultati
+    // =================================
+
+    for(long a : calcoli.get('a'))
+    {
+      for(long b : calcoli.get('b')) {
+        if (DiffieHellmanUtils.modPow(publicB, a, p) == DiffieHellmanUtils.modPow(publicA, b, p))
+        {
+          res.add((int) a);
+          res.add((int) b);
+          System.out.println("a = " + a);
+          System.out.println("b = " + b);
+        }
+      }
+    }
+
+    // =================================
+    // Ritorno i risultati
+    // =================================
+
     return res;
+  }
+}
+
+
+// =================================
+// Classe ausiliaria che compie i calcoli delle potenze di 'a' e 'b',
+// funzionante tramite Thread
+// =================================
+
+class Calcolatore extends Thread {
+
+  private long fromNumber, toNumber, keyP, keyG, resA, resB;
+  private HashMap<Character, List<Long>> calcoli;
+
+  public Calcolatore(long from, long to, long p, long g, long A, long B, HashMap<Character, List<Long>> calc)
+  {
+    this.fromNumber = from;
+    this.toNumber = to;
+    this.keyP = p;
+    this.keyG = g;
+    this.resA = A;
+    this.resB = B;
+    this.calcoli = calc;
+  }
+
+  @Override
+  public void run()
+  {
+    for(long x = fromNumber; x < toNumber; ++x)
+    {
+      if(DiffieHellmanUtils.modPow(keyG, x, keyP) == resA)
+        calcoli.get('a').add(x);
+
+      if(DiffieHellmanUtils.modPow(keyG, x, keyP) == resB)
+        calcoli.get('b').add(x);
+    }
   }
 }
